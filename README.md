6dd06e9c2ffd3a574f5c631ccf73513909074164
nisalVD <nisalvd@gmail.com>
2018-02-19 23:56:19 +1100

    add readme and changed name

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #CM-Template
+credits to https://github.com/deliciousbrains/wp-react-boilerplate for the template
--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ const webpackConfig = {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  const buildFolder = path.resolve(__dirname, 'wp-react-boilerplate-built')
+  const buildFolder = path.resolve(__dirname, 'cm-template')
   webpackConfig.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
       mangle: {
