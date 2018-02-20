e17192c1ecc96eaf159025ed1eb7555f3747d666
nisalVD <nisalvd@gmail.com>
2018-02-20 15:18:31 +1100

    changed component from container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App.js'
+import App from './containers/App.js'
 import ElementQueries from 'css-element-queries/src/ElementQueries'
 
 ElementQueries.listen()
