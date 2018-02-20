8c08717f741ae0524f50a35a5d848ebb1b2ba46f
Jared Hutchinson <jared.hutchinson99@gmail.com>
2018-02-20 20:07:22 +1100

    css template that sort of works on wordpress

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import PluginField from './PluginField'
+import './PluginField.scss'
 import './App.scss'
+
 class App extends Component {
   render() {
     return (
       <div className="hello-world">
-        <h1>Hello asdfasf</h1>
+        <h1>Is this thvbgcfg dfg dg dfsfdsd</h1>
+        <PluginField />
       </div>
     )
   }
