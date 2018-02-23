166a8e01b3fb7a5a286ee70c87e0e7a981b4d9ca
Mizuki Zenta <mizukisb@gmail.com>
2018-02-24 10:36:23 +1100

    try react chart

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,8 @@ import { getAlertConfig } from "../api/alertSetting"
 import moment from "moment"
 import FontAwesome from 'react-fontawesome'
 
+import { Line } from 'react-chartjs-2'
+
 class App extends Component {
   state = {
     currentDeviceData: null,
@@ -132,6 +134,7 @@ class App extends Component {
     this.setState({ selectedData })
   }
 
+
   render() {
     const {
       currentDeviceData,
@@ -141,8 +144,36 @@ class App extends Component {
       selectedData,
       errorType
     } = this.state
+
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
     return (
       !errorType ? <div className="plugin-container wrap center-text">
+        <Line data={data} />
 
         <div className='select-styling'>
           <select onChange={(e) => {
